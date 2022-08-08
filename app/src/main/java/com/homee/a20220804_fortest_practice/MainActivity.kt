e27ecb50c361a38259b2d4494019cbc963be5b4c
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-08 13:29:21 +0900

    Initial commit

---
+++ app/src/main/java/com/homee/a20220804_fortest_practice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.homee.a20220804_fortest_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
